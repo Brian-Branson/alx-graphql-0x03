d7d3fe41ccba9ecf60b05a885a82fdcddc46c057
Brian <karanjabrian382@gmail.com>
2025-06-30 19:38:14 +0300

    done

--- alx-rick-and-morty-app/pages/index.tsx
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -3,6 +3,18 @@ import { GET_EPISODES } from "@/graphql/queries"
 import { EpisodeProps } from "@/interfaces"
 import EpisodeCard from "@/components/common/EpisodeCard"
 import { useEffect, useState } from "react"
+import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorProneComponent from '@/components/ErrorProneComponent';
+
+const Home: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <ErrorProneComponent />
+      
+    </ErrorBoundary>
+    
+  );
+};
 
 
 
